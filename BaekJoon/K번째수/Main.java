1e91fb2fb7d15f6e724a7f8813489276525fc77e
JEONTAK <tak980418@naver.com>
2024-01-16 13:30:00 +0900

    BaekJoon : K번째 수(Gold1)

---
+++ BaekJoon/K번째수/Main.java
@@ -0,0 +1,28 @@
+package BaekJoon.K번째수;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        long cnt;
+        int left = 1, right = K, result = 0;
+        while (left <= right) {
+            cnt = 0;
+            int mid = (left + right) / 2;
+            for (int i = 1; i <= N; i++) {
+                cnt += Math.min(mid / i, N);
+            }
+            if (cnt < K) {
+                left = mid + 1;
+            } else {
+                result = mid;
+                right = mid - 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
