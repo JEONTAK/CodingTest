7b7de9cb205dd2b31538a513e01b8ba88b4bc4f0
JEONTAK <tak980418@naver.com>
2024-05-06 16:37:38 +0900

    BaekJoon : 2의 멱수의 합 (Gold 5)

--- BaekJoon/Gold5/_2의멱수의합/BOJ2410.java
+++ BaekJoon/Gold5/_2의멱수의합/BOJ2410.java
@@ -12,11 +12,10 @@ public class BOJ2410 {
     private static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        dp = new int[N + 1];
-        dp[0] = 0;
+        dp = new int[1_000_001];
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i <= N; i++) {
+        for (int i = 3; i < 1_000_001; i++) {
             if (i % 2 == 0) {
                 dp[i] = (dp[i - 1] + dp[i/2]) % 1_000_000_000;
             }else{
